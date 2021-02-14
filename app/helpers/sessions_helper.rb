c845e68d19dba33ea580debfd2d0babec7564cf4
toshiki-o23 <tsk.okmt.15@gmail.com>
2021-02-14 08:40:23 +0000

    Finish user edit, update, index, and destroy actions

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -23,9 +23,9 @@ module SessionsHelper
     end
   end
   
-  #ユーザーがログインしていればtrue、その他ならfalseを返す
-  def logged_in?
-    !current_user.nil?
+  # 渡されたユーザーがカレントユーザーであればtrueを返す
+  def current_user?(user)
+    user && user == current_user
   end
   
   # 永続的セッションを破棄する
---
+++ test/integration/users_edit_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class UsersEditTest < ActionDispatch::IntegrationTest
+
+  
+
+end
